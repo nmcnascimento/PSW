6ce8b3038a7f47543b39d493753d0f8fe697e514
nmcnascimento <nmcnascimento@gmail.com>
2022-11-11 11:37:25 +0000

    added table for visitor, cust, admin, catalog and order

--- OnlineShop/sql/tables.sql
+++ OnlineShop/sql/tables.sql
@@ -1,5 +1,31 @@
 CREATE TABLE visitor (
-  cust boolean ,
-  admin boolean ,
+  role boolean ,
   loginStatus boolean
 );
+
+CREATE TABLE Customer (
+  username varchar(100) NOT NULL,
+  password varchar(100) NOT NULL,
+  email varchar(100) NOT NULL UNIQUE,
+  balance float(53, 2) NOT NULL 
+);
+
+CREATE TABLE Admin (
+  username varchar(100) NOT NULL,
+  password varchar(100) NOT NULL
+);
+
+CREATE TABLE Catalog (
+  prodID varchar(50) NOT NULL UNIQUE,
+  prodName varchar(100) NOT NULL UNIQUE,
+  prodQty int(2) NOT NULL,
+  prodPrice float(5,2) NOT NULL,
+  prodTag varchar(50) NOT NULL UNIQUE
+);
+
+CREATE TABLE Order (
+  orderID int(2) NOT NULL,
+  orderDate DATE NOT NULL,
+  );
+
+
